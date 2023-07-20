14b91f2e2c325e331979fca666c9047c856d501e
Krins <krinskumar@gmail.com>
2023-07-20 13:48:41 -0400

    added-dashboard

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,9 +1,78 @@
+"use client"
+
+import { Code, ImageIcon, MessageSquare, Music, VideoIcon, ArrowRight } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+import { Card } from "@/components/ui/card"
+import { cn } from "@/lib/utils";
+
+const tools = [
+	{
+		label: 'Conversation',
+		icon: MessageSquare,
+		href: '/conversation',
+		color: "text-violet-500",
+		bgColor: "bg-violet-500/10",
+	},
+	{
+		label: 'Image Generation',
+		icon: ImageIcon,
+		color: "text-pink-700",
+		bgColor: "bg-pink-700/10",
+		href: '/image',
+	},
+	{
+		label: 'Video Generation',
+		icon: VideoIcon,
+		color: "text-orange-700",
+		bgColor: "bg-orange-700/10",
+		href: '/video',
+	},
+	{
+		label: 'Music Generation',
+		icon: Music,
+		href: '/music',
+		color: "text-emerald-500",
+		bgColor: "bg-emerald-500/10",
+	},
+	{
+		label: 'Code Generation',
+		icon: Code,
+		color: "text-green-700",
+		bgColor: "bg-green-700/10",
+		href: '/code',
+	},
+];
+
 const DashboardPage = () => {
+
+	const router = useRouter();
+
 	return (
 		<div>
-			<p className="text-6xl text-green-500">
-				Landing Page UP
-			</p>
+			<div className="mb-8 space-y-4">
+				<h2 className="text-2xl md:text-4xl font-bold text-center">
+					Explore the power of AI
+				</h2>
+				<p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+					Explore the collection of AI tools that can improve your workflow.
+				</p>
+			</div>
+			<div className="px-4 md:px-20 lg:px-32 space-y-4">
+				{tools.map((tool) => (
+					<Card key={tool.href} className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"onClick={() => {router.push(tool.href)}}>
+						<div className="flex items-center gap-x-4">
+							<div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+								<tool.icon className={cn("w-8 h-8", tool.color)} />
+							</div>
+							<div className="font-semibold">
+								{tool.label}
+							</div>
+						</div>
+						<ArrowRight className="w-5 h-5"/>
+					</Card>
+				))}
+			</div>
 		</div>
 	)
 }
