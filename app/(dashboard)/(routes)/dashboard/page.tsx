5cd484223e8325a32e763c190de19c9c912c7d8b
Krins <krinskumar@gmail.com>
2023-07-20 13:19:31 -0400

    sidebar-added

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,12 +1,9 @@
-import { UserButton } from "@clerk/nextjs";
-
 const DashboardPage = () => {
 	return (
 		<div>
 			<p className="text-6xl text-green-500">
 				Landing Page UP
 			</p>
-			<UserButton afterSignOutUrl="/"/>
 		</div>
 	)
 }
