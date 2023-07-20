6832d44c74a1d4dceb81dc7cfec9e5bc60f9fc30
Krins <krinskumar@gmail.com>
2023-07-20 13:29:05 -0400

    current-sidebar-fixed

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -55,6 +55,9 @@ const routes = [
 ]
 
 const Sidebar = () => {
+
+    const pathname = usePathname();
+
     return (
         <div className="space-y-4 p-4 flex flex-col h-full bg-[#111827] text-white">
             <div className="px-3 py-2 flex-1">
@@ -68,7 +71,7 @@ const Sidebar = () => {
                 </Link>
                 <div className="space-y-1">
                     {routes.map((route) => (
-                        <Link href={route.href} key={route.href} className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition">
+                        <Link href={route.href} key={route.href} className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition", pathname === route.href ? "text-white bg-white/10" : "text-zinc-400")}>
                             <div className="flex items-center flex-1">
                                 <route.icon className={cn("w-5 h-5 mr-3", route.color)}/>
                                 {route.label}
